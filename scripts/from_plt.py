2092d9f7d355af42f4c5d12139ab593249d8a231
Alex <alexanderw@uvic.ca>
2026-03-31 12:47:13 -0700

    moved stb_image to libs folder

--- scripts/from_plt.py
+++ scripts/from_plt.py
@@ -5,13 +5,16 @@ from PIL import Image
 num_colors = 256
 
 def main():
-    map = plt.colormaps.get_cmap("twilight")
+    colname = "hsv"
+
+
+    map = plt.colormaps.get_cmap(colname)
 
     colors = np.ndarray([1,num_colors,3])
     colors[0] = map(np.linspace(0,1,num_colors))[:,:3]
     
     output = np.array(colors * num_colors, dtype=np.uint8)
-    Image.fromarray(output, "RGB").save("twilight.png")
+    Image.fromarray(output, "RGB").save(colname+".png")
 
     
 
--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 #include "state.hpp"
 
 #define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
+#include <stb/stb_image.h>
 
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
